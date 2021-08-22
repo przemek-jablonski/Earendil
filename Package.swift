09ed9c784216abb81b5a0a4fb4fd6606ea76cdd0
Przemysław Jabłoński <przjablonski@gmail.com>
2021-08-22 14:26:26 +0200

    added lower-bound platform requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Earendil",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+        .tvOS(.v14),
+        .watchOS(.v7)
+    ],
     products: [
         .library(name: "Earendil",
                  type: .static,
